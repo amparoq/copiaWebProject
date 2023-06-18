47e6e742aa1b9145cebbec4d4bca0e3be4b0e5ef
amparoq <aquiroz@miuandes.cl>
2023-06-18 01:10:08 -0400

    change role listo creo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,13 +43,26 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if params[:user][:commit] == "role"
+      @user.role = params[:user][:role].to_i
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to "/users/admin_board", notice: "User was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
